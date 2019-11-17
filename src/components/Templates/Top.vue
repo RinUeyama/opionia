cfe5702a62e08ede7514a3fc6f4426034d1d42da
Opion <kindledkindred@gmail.com>
2019-11-17 20:17:30 +0900

    Topのレイアウトを暫定的に変更

--- src/components/Templates/Top.vue
+++ src/components/Templates/Top.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
-  #top
-    h1 OPIONIA
+  v-container(fluid)
+    v-row.mt-4.pa-4(justify="center")
+      h1.display-4 OPIONIA
+    v-row.pa-4(justify="center")
+      p ただいま改装中
 </template>
 
 <script lang="ts">
