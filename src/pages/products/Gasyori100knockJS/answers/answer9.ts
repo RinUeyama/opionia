a5bbca846dc7dd080cdce14e7eed093ad05ea57a
Opion <kindledkindred@gmail.com>
2019-12-01 00:03:31 +0900

    bug: Answer9

--- src/pages/products/Gasyori100knockJS/answers/answer9.ts
+++ src/pages/products/Gasyori100knockJS/answers/answer9.ts
@@ -9,5 +9,35 @@ export default function gaussianFilter (
   canvas: HTMLCanvasElement
 ): void {
   const srcTensor: tf.Tensor<tf.Rank.R3> = tf.browser.fromPixels(src)
-  tf.browser.toPixels(srcTensor, canvas)
+
+  const gaussianKernel = (
+    ksize: number,
+    sigma: number
+  ) => {
+    if (ksize % 2 === 0) {
+      console.error(`ksize is inputed ${ksize}. ksize must be odd.`)
+    }
+
+    const x: tf.Tensor<tf.Rank.R1> = tf.linspace(-sigma, sigma, ksize)
+    const y: tf.Tensor<tf.Rank.R1> = tf.linspace(-sigma, sigma, ksize)
+    console.log(`x, y: ${x}, ${y}`)
+    const kernel = tf.exp(-(tf.square(x).add(tf.square(y))).floorDiv(2 * Math.pow(sigma, 2))).floorDiv(Math.sqrt(2 * Math.PI) * sigma)
+
+    return kernel
+  }
+
+  console.log(`kernel: ${gaussianKernel(3, 1.3)}`)
+
+  const kernel = gaussianKernel(3, 1.3)
+
+  const padTensor = srcTensor.pad([[1, 1], [1, 1]])
+  let dstTensor = tf.zeros([src.height, src.width, 3]).pad([[1, 1], [1, 1]])
+
+  for (let i = 0; i < src.width; i++) {
+    for (let j = 0; j < src.height; j++) {
+      tf.add(dstTensor.slice([i, j], [3, 3]), padTensor.slice([i, j], [3, 3]).matMul(kernel))
+    }
+  }
+
+  console.log(`Dst after for: ${dstTensor}`)
 }
